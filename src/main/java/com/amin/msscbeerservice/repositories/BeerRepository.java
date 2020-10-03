b24bd1263e0e8546e4a0992932328003a42e5ffd
Amin <amin.mb37@gmail.com>
2020-10-03 15:16:25 +0200

    Adding beer repository. Closes #6

---
+++ src/main/java/com/amin/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.amin.msscbeerservice.repositories;
+
+import com.amin.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
