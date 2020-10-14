c33903857c809dd0a5c715dc2473fc4345355e6b
aminmb37 <amin.mb37@gmail.com>
2020-10-15 01:09:30 +0200

    Adding methods for BeerController. Closes #18.

--- src/main/java/com/amin/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/amin/msscbeerservice/web/controller/BeerController.java
@@ -1,6 +1,8 @@
 package com.amin.msscbeerservice.web.controller;
 
+import com.amin.msscbeerservice.services.BeerService;
 import com.amin.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -10,23 +12,23 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/beer")
+@RequiredArgsConstructor
 public class BeerController {
+    private final BeerService beerService;
+
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
-        //todo impl
-        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+        return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<BeerDto> saveNewBeer(@Validated @RequestBody BeerDto beerDto) {
-        //todo impl
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>(beerService.saveNewBeer(beerDto), HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
     public ResponseEntity<BeerDto> updateBeerById(@PathVariable("beerId") UUID beerId,
                                                   @Validated @RequestBody BeerDto beerDto) {
-        //todo impl
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(beerService.updateBeer(beerId, beerDto), HttpStatus.NO_CONTENT);
     }
 }
