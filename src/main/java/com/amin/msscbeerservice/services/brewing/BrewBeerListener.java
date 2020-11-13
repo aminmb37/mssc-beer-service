d29a706d07bda27e809763927e846c0912055885
aminmb37 <amin.mb37@gmail.com>
2020-11-13 23:14:43 +0100

    Changing getOne to findById.

--- src/main/java/com/amin/msscbeerservice/services/brewing/BrewBeerListener.java
+++ src/main/java/com/amin/msscbeerservice/services/brewing/BrewBeerListener.java
@@ -13,6 +13,8 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -24,10 +26,12 @@ public class BrewBeerListener {
     @JmsListener(destination = JmsConfig.BREWING_REQUEST_QUEUE)
     public void listen(BrewBeerEvent brewBeerEvent) {
         BeerDto beerDto = brewBeerEvent.getBeerDto();
-        Beer beer = beerRepository.getOne(beerDto.getId());
-        beerDto.setQuantityOnHand(beer.getQuantityToBrew());
-        NewInventoryEvent newInventoryEvent = new NewInventoryEvent(beerDto);
-        log.debug("Brewed beer: " + beer.getMinOnHand() + " : QOH: " + beerDto.getQuantityOnHand());
-        jmsTemplate.convertAndSend(JmsConfig.NEW_INVENTORY_QUEUE, newInventoryEvent);
+        Optional<Beer> beerOptional = beerRepository.findById(beerDto.getId());
+        beerOptional.ifPresent(beer -> {
+            beerDto.setQuantityOnHand(beer.getQuantityToBrew());
+            NewInventoryEvent newInventoryEvent = new NewInventoryEvent(beerDto);
+            log.debug("Brewed beer: " + beer.getMinOnHand() + " : QOH: " + beerDto.getQuantityOnHand());
+            jmsTemplate.convertAndSend(JmsConfig.NEW_INVENTORY_QUEUE, newInventoryEvent);
+        });
     }
 }
