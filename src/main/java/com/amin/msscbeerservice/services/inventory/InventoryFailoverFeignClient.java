521f46479606e0d1444e0b99952beb785414121f
aminmb37 <amin.mb37@gmail.com>
2020-11-21 02:31:51 +0100

    Adding Inventory Failover Feign Client Service.

---
+++ src/main/java/com/amin/msscbeerservice/services/inventory/InventoryFailoverFeignClient.java
@@ -0,0 +1,15 @@
+package com.amin.msscbeerservice.services.inventory;
+
+import com.amin.msscbeerservice.services.inventory.model.BeerInventoryDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@FeignClient("inventory-failover")
+public interface InventoryFailoverFeignClient {
+    @RequestMapping(method = RequestMethod.GET, value = "/inventory-failover")
+    ResponseEntity<List<BeerInventoryDto>> getOnHandInventory();
+}
---
+++ src/main/java/com/amin/msscbeerservice/services/inventory/InventoryServiceFeignClientFailover.java
@@ -0,0 +1,20 @@
+package com.amin.msscbeerservice.services.inventory;
+
+import com.amin.msscbeerservice.services.inventory.model.BeerInventoryDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class InventoryServiceFeignClientFailover implements InventoryServiceFeignClient {
+    private final InventoryFailoverFeignClient inventoryFailoverFeignClient;
+
+    @Override
+    public ResponseEntity<List<BeerInventoryDto>> getOnHandInventory(UUID beerId) {
+        return inventoryFailoverFeignClient.getOnHandInventory();
+    }
+}
