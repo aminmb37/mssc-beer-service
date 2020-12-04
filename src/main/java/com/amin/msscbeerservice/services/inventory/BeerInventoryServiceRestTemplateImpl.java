1de449479c9d55f1035a7c27097a9bda00119476
aminmb37 <amin.mb37@gmail.com>
2020-12-04 03:10:55 +0100

    Configuring HTTP Basic Authentication for RESTTemplate.

--- src/main/java/com/amin/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImpl.java
+++ src/main/java/com/amin/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImpl.java
@@ -2,6 +2,7 @@ package com.amin.msscbeerservice.services.inventory;
 
 import com.amin.msscbeerservice.services.inventory.model.BeerInventoryDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Profile;
@@ -18,15 +19,18 @@ import java.util.UUID;
 @Component
 @Slf4j
 @Profile("!local-discovery")
-@ConfigurationProperties(prefix = "amin.brewery", ignoreUnknownFields = false)
+@ConfigurationProperties(prefix = "amin.brewery", ignoreUnknownFields = true)
 public class BeerInventoryServiceRestTemplateImpl implements BeerInventoryService {
     public static final String INVENTORY_PATH = "api/v1/beer/{beerId}/inventory";
 
     private String beerInventoryServiceHost;
+
     private final RestTemplate restTemplate;
 
-    public BeerInventoryServiceRestTemplateImpl(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
+    public BeerInventoryServiceRestTemplateImpl(RestTemplateBuilder restTemplateBuilder,
+                                                @Value("${amin.brewery.inventory-user}") String inventoryUser,
+                                                @Value("${amin.brewery.inventory-password}") String inventoryPassword) {
+        this.restTemplate = restTemplateBuilder.basicAuthentication(inventoryUser, inventoryPassword).build();
     }
 
     public void setBeerInventoryServiceHost(String beerInventoryServiceHost) {
